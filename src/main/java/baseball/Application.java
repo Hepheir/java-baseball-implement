a52c9dcae4f130bb5310eaab136dabe0b9693c6e
Hepheir <hepheir@gmail.com>
2022-03-16 02:31:06 +0900

    feat(Application): make `Application` implement `Game`

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
-public class Application {
+public class Application implements Game {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        Application app = new Application();
+        app.start();
     }
 }
 
