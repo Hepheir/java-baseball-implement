74c53963f992053369f9d0e2d1d695f57a5b8612
Hepheir <hepheir@gmail.com>
2022-03-15 18:50:55 +0900

    feat(Application): add interface `Game`

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,3 +5,35 @@ public class Application {
         //TODO: 숫자 야구 게임 구현
     }
 }
+
+interface Game {
+    public int getHostNumber(int index);
+
+    public int setHostNumber(int index, int value);
+
+    public int getPlayerNumber(int index);
+
+    public int setPlayerNumber(int index, int value);
+
+    default public void start() {
+        setup();
+        while (!isWin()) {
+            move();
+        }
+        end();
+    }
+
+    public void setup();
+
+    public void move();
+
+    public int countStrikes();
+
+    public int countBalls();
+
+    public void printResult();
+
+    public boolean isWin();
+
+    public void end();
+}
