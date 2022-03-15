e3f08419326dc61a69f6abaac8cc73b3a0a730ed
Hepheir <hepheir@gmail.com>
2022-03-16 02:43:30 +0900

    feat(Application): implement getters setters of host and player numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,45 @@
 package baseball;
 
 public class Application implements Game {
+    int[] hostNumbers = {-1, -1, -1};
+    int[] playerNumbers = {-1, -1, -1};
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         Application app = new Application();
         app.start();
+    }
+
+    @Override
+    public int getHostNumber(int index) {
+        assert checkIndex(index);
+        return hostNumbers[index];
+    }
+
+    @Override
+    public void setHostNumber(int index, int value) {
+        assert checkIndex(index) && checkValue(value);
+        hostNumbers[index] = value;
+    }
+
+    @Override
+    public int getPlayerNumber(int index) {
+        assert checkIndex(index);
+        return playerNumbers[index];
+    }
+
+    @Override
+    public void setPlayerNumber(int index, int value) {
+        assert checkIndex(index) && checkValue(value);
+        playerNumbers[index] = value;
+    }
+
+    private boolean checkIndex(int index) {
+        return 0 <= index && index <= 2;
+    }
+
+    private boolean checkValue(int value) {
+        return 1 <= value && value <= 9;
     }
 }
 
