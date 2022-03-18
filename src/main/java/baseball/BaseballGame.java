75ac7b6a3175778434e754f9a5d4f6f41e0f481d
Hepheir <hepheir@gmail.com>
2022-03-18 15:29:19 +0900

    refactor(Application, BaseballGame): rewrite program to make `Application` runs game **only**

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,17 +1,86 @@
 package baseball;
 
-public interface BaseballGame extends Game {
-    public int getHostNumber(int index);
+import java.util.ArrayList;
 
-    public void setHostNumber(int index, int value);
+import camp.nextstep.edu.missionutils.Randoms;
 
-    public int getPlayerNumber(int index);
+public class BaseballGame implements Game {
+    private final int AMOUNT_OF_NUMBERS = 3;
+    private final int NUMBER_RANGE_START_INCLUSIVE = 1;
+    private final int NUMBER_RANGE_END_INCLUSIVE = 9;
+    private ArrayList<Integer> hostNumbers;
+    private ArrayList<Integer> playerNumbers;
 
-    public void setPlayerNumber(int index, int value);
+    @Override
+    public void onStart() {
+        setup();
+    }
 
-    public int countStrikes();
+    private void setup() {
+        hostNumbers = generateRandomNumbers();
+    }
 
-    public int countBalls();
+    private ArrayList<Integer> generateRandomNumbers() {
+        ArrayList<Integer> generatedNumbers = new ArrayList<>();
+        Randoms.pickUniqueNumbersInRange(
+                NUMBER_RANGE_START_INCLUSIVE,
+                NUMBER_RANGE_END_INCLUSIVE,
+                AMOUNT_OF_NUMBERS)
+            .forEach((n) -> {
+                generatedNumbers.add(n);
+            });;
+        return generatedNumbers;
+    }
 
-    public void printResult();
+    private int countBalls() {
+        int ballsCnt = 0;
+        for (Integer number : playerNumbers) {
+            if (hostNumbers.contains(number)) {
+                ballsCnt++;
+            }
+        }
+        ballsCnt -= countStrikes();
+        return ballsCnt;
+    }
+
+    private int countStrikes() {
+        int strikesCnt = 0;
+        for (int index = 0; index < AMOUNT_OF_NUMBERS; index++) {
+            if (hostNumbers.get(index) == playerNumbers.get(index)) {
+                strikesCnt++;
+            }
+        }
+        return strikesCnt;
+    }
+
+    @Override
+    public void onTurn() throws IllegalArgumentException {
+        playerNumbers = Control.getNIntegersInRange(
+                NUMBER_RANGE_START_INCLUSIVE,
+                NUMBER_RANGE_END_INCLUSIVE,
+                AMOUNT_OF_NUMBERS);
+        int strikes = countStrikes();
+        int balls = countBalls();
+        View.printStatistic(strikes, balls);
+    }
+
+    @Override
+    public boolean isWin() {
+        return countStrikes() == AMOUNT_OF_NUMBERS;
+    }
+
+    @Override
+    public void onEnd() {
+        View.printGameEndMessage(AMOUNT_OF_NUMBERS);
+    }
+
+    public void runMultipleGames() {
+        while (true) {
+            run();
+            View.printHelpMessage();
+            if (Control.getCommandFromInput() == Command.END_GAME) {
+                break;
+            }
+        };
+    }
 }
